12954a31f82212c997c10976fe53009f5b4a2716
h0rak <trey.horak@gmail.com>
2023-03-15 20:53:20 -0500

    renamed variables. lambdas to replace onclick listener on buttons

--- app/src/main/java/com/example/teamrandomizer/MainActivity.java
+++ app/src/main/java/com/example/teamrandomizer/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.teamrandomizer;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -23,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
     TextView team3;
     TextView team4;
     String[] englishTeams;
-    String[] spainTeams;
-    String[] italyTeams;
-    String[] germanyTeams;
-    String[] franceTeams;
+    String[] spanishTeams;
+    String[] italianTeams;
+    String[] germanTeams;
+    String[] frenchTeams;
     String[] internationalTeams;
 
     @Override
@@ -44,78 +43,72 @@ public class MainActivity extends AppCompatActivity {
         team3 = findViewById(R.id.team3TextView);
         team4 = findViewById(R.id.team4TextView);
         englishTeams = getResources().getStringArray(R.array.eng_teams);
-        spainTeams = getResources().getStringArray(R.array.spa_teams);
-        italyTeams = getResources().getStringArray(R.array.ita_teams);
-        germanyTeams = getResources().getStringArray(R.array.ger_teams);
-        franceTeams = getResources().getStringArray(R.array.fra_teams);
+        spanishTeams = getResources().getStringArray(R.array.spa_teams);
+        italianTeams = getResources().getStringArray(R.array.ita_teams);
+        germanTeams = getResources().getStringArray(R.array.ger_teams);
+        frenchTeams = getResources().getStringArray(R.array.fra_teams);
         internationalTeams = getResources().getStringArray(R.array.int_teams);
 
-        threeTeamButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (leagueSpinner.getSelectedItemPosition()) {
-                    case 0:
-                        Toast.makeText(MainActivity.this, "Please select a league/country", Toast.LENGTH_LONG).show();
-                        break;
-                    case 1:
-                        team1.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
-                        team2.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
-                        team3.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
-                        break;
-                    case 2:
-                        team1.setText(spainTeams[new Random().nextInt(spainTeams.length)]);
-                        team2.setText(spainTeams[new Random().nextInt(spainTeams.length)]);
-                        team3.setText(spainTeams[new Random().nextInt(spainTeams.length)]);
-                        break;
-                    case 3:
-                        team1.setText(italyTeams[new Random().nextInt(italyTeams.length)]);
-                        team2.setText(italyTeams[new Random().nextInt(italyTeams.length)]);
-                        team3.setText(italyTeams[new Random().nextInt(italyTeams.length)]);
-                        break;
-                    case 4:
-                        team1.setText(germanyTeams[new Random().nextInt(germanyTeams.length)]);
-                        team2.setText(germanyTeams[new Random().nextInt(germanyTeams.length)]);
-                        team3.setText(germanyTeams[new Random().nextInt(germanyTeams.length)]);
-                        break;
-                    case 5:
-                        team1.setText(franceTeams[new Random().nextInt(franceTeams.length)]);
-                        team2.setText(franceTeams[new Random().nextInt(franceTeams.length)]);
-                        team3.setText(franceTeams[new Random().nextInt(franceTeams.length)]);
-                        break;
-                    case 6:
-                        team1.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
-                        team2.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
-                        team3.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
-                        break;
-                }
+        threeTeamButton.setOnClickListener(v -> {
+            switch (leagueSpinner.getSelectedItemPosition()) {
+                case 0:
+                    Toast.makeText(MainActivity.this, "Please select a league/country", Toast.LENGTH_LONG).show();
+                    break;
+                case 1:
+                    team1.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
+                    team2.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
+                    team3.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
+                    break;
+                case 2:
+                    team1.setText(spanishTeams[new Random().nextInt(spanishTeams.length)]);
+                    team2.setText(spanishTeams[new Random().nextInt(spanishTeams.length)]);
+                    team3.setText(spanishTeams[new Random().nextInt(spanishTeams.length)]);
+                    break;
+                case 3:
+                    team1.setText(italianTeams[new Random().nextInt(italianTeams.length)]);
+                    team2.setText(italianTeams[new Random().nextInt(italianTeams.length)]);
+                    team3.setText(italianTeams[new Random().nextInt(italianTeams.length)]);
+                    break;
+                case 4:
+                    team1.setText(germanTeams[new Random().nextInt(germanTeams.length)]);
+                    team2.setText(germanTeams[new Random().nextInt(germanTeams.length)]);
+                    team3.setText(germanTeams[new Random().nextInt(germanTeams.length)]);
+                    break;
+                case 5:
+                    team1.setText(frenchTeams[new Random().nextInt(frenchTeams.length)]);
+                    team2.setText(frenchTeams[new Random().nextInt(frenchTeams.length)]);
+                    team3.setText(frenchTeams[new Random().nextInt(frenchTeams.length)]);
+                    break;
+                case 6:
+                    team1.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
+                    team2.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
+                    team3.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
+                    break;
             }
         });
 
-        oneTeamButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (leagueSpinner.getSelectedItemPosition()) {
-                    case 0:
-                        break;
-                    case 1:
-                        team4.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
-                        break;
-                    case 2:
-                        team4.setText(spainTeams[new Random().nextInt(spainTeams.length)]);
-                        break;
-                    case 3:
-                        team4.setText(italyTeams[new Random().nextInt(italyTeams.length)]);
-                        break;
-                    case 4:
-                        team4.setText(germanyTeams[new Random().nextInt(germanyTeams.length)]);
-                        break;
-                    case 5:
-                        team4.setText(franceTeams[new Random().nextInt(franceTeams.length)]);
-                        break;
-                    case 6:
-                        team4.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
-                        break;
-                }
+        oneTeamButton.setOnClickListener(v -> {
+            switch (leagueSpinner.getSelectedItemPosition()) {
+                case 0:
+                    break;
+                case 1:
+                    team4.setText(englishTeams[new Random().nextInt(englishTeams.length)]);
+                    break;
+                case 2:
+                    team4.setText(spanishTeams[new Random().nextInt(spanishTeams.length)]);
+                    break;
+                case 3:
+                    team4.setText(italianTeams[new Random().nextInt(italianTeams.length)]);
+                    break;
+                case 4:
+                    team4.setText(germanTeams[new Random().nextInt(germanTeams.length)]);
+                    break;
+                case 5:
+                    team4.setText(frenchTeams[new Random().nextInt(frenchTeams.length)]);
+                    break;
+                case 6:
+                    team4.setText(internationalTeams[new Random().nextInt(internationalTeams.length)]);
+                    break;
             }
         });
     }
